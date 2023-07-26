d8bb7a89574f8289d69d70d1178978aa73338324
Nanashi213 <sebastian.agudelo2@utp.edu.co>
2023-07-26 16:38:26 -0500

    Add jump endpoints

--- router/jump.router.js
+++ router/jump.router.js
@@ -1,13 +1,21 @@
 const router = require("express").Router();
+const { config } = require("../config");
+const GameService = require("../service/game.service.js");
+
+const gameService = new GameService();
 
 
-router.put("/right", (req, res) => {
-  res.json({ message: "Jump right" });
+router.put("/right", async (req, res) => {
+  const data = {"x":10,"y":10,"attacking":0,"jumping":true }
+  await gameService.updateGameStatus(config.nameFile, data);
+  res.json({"Data_update":data});
 }
 );
 
-router.put("/left", (req, res) => {
-  res.json({ message: "Jump left" });
+router.put("/left", async (req, res) => {
+  const data = {"x":-10,"y":10,"attacking":0,"jumping":true }
+  await gameService.updateGameStatus(config.nameFile, data);
+  res.json({"Data_update":data});
 }
 );
 
