106c0a1c68171b3c13901c99706b9f2c74259522
Nanashi213 <sebastian.agudelo2@utp.edu.co>
2023-07-26 16:49:52 -0500

    Add status code jump

--- router/jump.router.js
+++ router/jump.router.js
@@ -8,14 +8,14 @@ const gameService = new GameService();
 router.put("/right", async (req, res) => {
   const data = {"x":10,"y":10,"attacking":0,"jumping":true }
   await gameService.updateGameStatus(config.nameFile, data);
-  res.json({"Data_update":data});
+  res.json({"Data_update":data}).status(200);
 }
 );
 
 router.put("/left", async (req, res) => {
   const data = {"x":-10,"y":10,"attacking":0,"jumping":true }
   await gameService.updateGameStatus(config.nameFile, data);
-  res.json({"Data_update":data});
+  res.json({"Data_update":data}).status(200);
 }
 );
 
